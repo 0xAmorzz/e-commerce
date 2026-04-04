6b5a9c2903e32e2ad8b25786d8c1d47e60b3b80d
Amr Mohamed <amrmohm629@gmail.com>
2026-04-04 21:39:03 +0200

    (feat): configure user endpoints

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from drf_spectacular.views import extend_schema
+
 
 # Create your views here.
+
+class UserRegistrationView(APIView):
+    @extend_schema(request=UserRegistrationSerializer, responses={201: UserRegistrationSerializer})
+
+    # post method to handle user registeration
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({'refresh': str(refresh),'access': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
