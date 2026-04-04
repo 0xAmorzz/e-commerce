0e761af559f1be638e424b4af93aeed4eefc4a4b
Amr Mohamed <amrmohm629@gmail.com>
2026-04-04 21:39:17 +0200

    (feat): configure user endpoints

---
+++ users/urls.py
@@ -0,0 +1,20 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView
+)
+
+from django.urls import path
+from .views import UserRegistrationView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
+
