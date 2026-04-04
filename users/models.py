5f877bc57782ad4b295863509773d68689370b79
Amr Mohamed <amrmohm629@gmail.com>
2026-04-04 21:37:48 +0200

    (feat): create custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.core.validators import RegexValidator
 # Create your models here.
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=50) # will change it to choices later
+    gender = models.CharField(choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+    phone_number = models.CharField(max_length=20,validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$',
+                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+            )
+        ]
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
