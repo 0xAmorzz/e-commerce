0a89f9d51336377bc656ef0c928238335e71a1a7
Eldax <mohamedabu.el3ela@gmail.com>
2026-04-06 01:56:12 +0200

    (feat): adding brands , variants logic

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 # specify the genders for each product (ex: Men , Women , Kids)
 class ProductGender(models.Model):
-    name = models.TextField(max_length=10)
+    name = models.CharField(max_length=10)
 
 
 
@@ -14,16 +14,26 @@ class ProductGender(models.Model):
 # there could be duplicates since a category for both men and women have different images
 class Category(models.Model):
     name = models.TextField()
-    image = models.ImageField(upload_to='/products/categories/images' , null=True , blank=True)
+    image = models.ImageField(upload_to='products/categories/images' , null=True , blank=True)
     gender = models.ForeignKey(ProductGender , on_delete=models.CASCADE , related_name='categories')
 
 
 
+
+
+# for each brand there are multiple products of that brand
+# brand -> many products(fk)
+class ProductBrand(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
 # category -> many products(fk) 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name='products')
-    description = models.CharField()
+    description = models.TextField(blank=True)
+    brand = models.ForeignKey(ProductBrand , null=True , on_delete=models.SET_NULL)
 
 
 
@@ -42,22 +52,34 @@ class ProductSize(models.Model):
 
 
 
+
+
+
 # for each product there are variants with different colors , prices
 class ProductItem(models.Model):
     product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name='items')
-    color = models.ForeignKey(ProductColor , on_delete=models.SET_NULL , related_name='colors')
-    size = models.ForeignKey(ProductSize , on_delete=models.CASCADE , related_name='sizes')
+    color = models.ForeignKey(ProductColor , null=True , on_delete=models.SET_NULL , related_name='items')
     original_price = models.DecimalField(max_digits=8 , decimal_places=2)
     sale_price = models.DecimalField(max_digits= 8 , decimal_places=2 , null=True , blank=True)
+
+    class Meta:
+        unique_together = [['product' , 'color']]
+
+
+
+
+# for each product item there are variations with sizes
+
+class ProductVariation(models.Model):
+    prd_item = models.ForeignKey(ProductItem , on_delete=models.CASCADE)
+    size = models.ForeignKey(ProductSize , on_delete=models.CASCADE , related_name='variations')
     stock = models.IntegerField()
-
-
 
 
 
 # since for each product there could be multiple images ,
 # thus we create a table for them
-# product -> many images(fk)
+# product_item -> many images(fk)
 class ProductImage(models.Model):
     image = models.ImageField(upload_to='/products/images' , null=True , blank=True)
     product = models.ForeignKey(ProductItem , on_delete=models.CASCADE , related_name='images')
