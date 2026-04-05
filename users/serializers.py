1be67ba0d9648d3e06b6fc4c59721c5c50f93d97
Amr Mohamed <amrmohm629@gmail.com>
2026-04-05 20:18:58 +0200

    (fix): handle creating users with the create_user method

--- users/serializers.py
+++ users/serializers.py
@@ -13,14 +13,20 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password', 'password2', 'country','phone_number','gender']
+        fields = ['first_name', 'last_name', 'username', 'password', 'password2', 'country','phone_number','gender']
 
     def validate(self, data):
         if data['password'] != data['password2']:
-            return serializers.ValidationError("Passwords do not match.")
+            raise serializers.ValidationError("Passwords do not match.")
         return data
     
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(username=validated_data['username'],
+                                        password=validated_data['password'],
+                                        first_name=validated_data['first_name'],
+                                        last_name=validated_data['last_name'],
+                                        country=validated_data['country'],
+                                        phone_number=validated_data['phone_number'],
+                                        gender=validated_data['gender'])
         return user
