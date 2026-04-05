83de3c34374f32f981dcc41ddc219eb92b2c51cb
Amr Mohamed <amrmohm629@gmail.com>
2026-04-05 23:06:56 +0200

    (feat): create the end point that creates access and refresh tokens if user is logged in with google

--- users/views.py
+++ users/views.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from drf_spectacular.views import extend_schema
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
+
 
 
 # Create your views here.
@@ -21,3 +25,8 @@ class UserRegistrationView(APIView):
             return Response({'refresh': str(refresh),'access': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+@api_view(['GET'])
+def get_tokens(request):
+    access_token = request.session.get('access_token')
+    refresh_token = request.session.get('refresh_token')
+    return Response({'access_token': access_token, 'refresh_token': refresh_token}, status=status.HTTP_200_OK)
