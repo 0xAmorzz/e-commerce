169c30cdc542983e34d22eca2558409badb93bd4
Amr Mohamed <amrmohm629@gmail.com>
2026-04-05 20:17:00 +0200

    (feat): create customusermanager to handle creating users as admins or normal users and add indexing to the email field

--- users/models.py
+++ users/models.py
@@ -1,15 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.core.validators import RegexValidator
 # Create your models here.
 
-class User(AbstractBaseUser):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username=None, password=None, **extra_fields):
+        if username is None:
+            raise ValueError('Users must have an email address')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self , username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must have is_superuser=True')
+        return self.create_user(username, password, **extra_fields)
+        
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = CustomUserManager()
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True, db_index=True)
+    username = models.EmailField(unique=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     country = models.CharField(max_length=50) # will change it to choices later
-    gender = models.CharField(choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+    gender = models.CharField(choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')], max_length=10)
     phone_number = models.CharField(max_length=20,validators=[
             RegexValidator(
                 regex=r'^\+?1?\d{9,15}$',
@@ -19,3 +38,5 @@ class User(AbstractBaseUser):
     )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    USERNAME_FIELD = 'username'
