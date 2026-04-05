965198f3034a0bc2ad7d9de2cb11e1fd31927d1f
Amr Mohamed <amrmohm629@gmail.com>
2026-04-06 00:25:07 +0200

    (feat): configure urls for the login, logout and refresh token endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,13 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenBlacklistView
-)
-
 from django.urls import path
-from .views import UserRegistrationView, get_tokens
+from .views import UserRegistrationView,UserLoginView,UserLogoutView,UserTokenRefreshView, get_tokens
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('token/refresh/', UserTokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('social-login/', get_tokens, name='social_login'),
