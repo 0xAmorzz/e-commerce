269c9127f3442360a2b26d98a9e34d97aa2d5415
Amr Mohamed <amrmohm629@gmail.com>
2026-04-06 00:23:45 +0200

    (feat): create the serializer for the login

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import authenticate
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,7 +14,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password', 'password2', 'country','phone_number','gender']
+        fields = ['first_name', 'last_name', 'email', 'password', 'password2', 'country','phone_number','gender']
 
     def validate(self, data):
         if data['password'] != data['password2']:
@@ -22,7 +23,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(username=validated_data['username'],
+        user = User.objects.create_user(email=validated_data['email'],
                                         password=validated_data['password'],
                                         first_name=validated_data['first_name'],
                                         last_name=validated_data['last_name'],
@@ -30,3 +31,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
                                         phone_number=validated_data['phone_number'],
                                         gender=validated_data['gender'])
         return user
+    
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self,data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid email or password")
